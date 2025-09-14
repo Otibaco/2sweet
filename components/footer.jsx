900922e1fe88af71410434cb9f61df424e005dd8
Otibaco <orjifranklin42@gmail.com>
2025-09-14 12:24:59 +0100

    latest update

--- components/footer.jsx
+++ components/footer.jsx
@@ -100,7 +100,7 @@ export function Footer() {
               <Image
                 src="/payment-image1.webp"
                 alt="Accepted payment methods"
-                width={250}
+                width={150}
                 height={40}
                 className="h-8 w-auto"
               />
